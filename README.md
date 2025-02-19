c08f3647b4933a6b3aa85f7303a9576f88944e18
hugosczs <hugoss927@gmail.com>
2025-02-18 18:53:09 -0600

    Define esqueleto de la clase abstracta Progresión.java

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Archivo de prueba
+
+Comando para compilar Boolean: javac -d target **/**/*.java
+
+Comando para comilar Progresión: javac -d target **/**/**/*.java
---
+++ unam/fciencias/math/Progresion.java
@@ -0,0 +1,34 @@
+package edd.unam.fciencias.math.Progresión.java;
+
+public abstract class Progresion {
+
+    /*
+     * Valor actual de la progresión
+     */
+
+     /*
+      * Constructor de la clase
+      */
+
+      /*
+       * Método que devuelve el valor actual de la progresión
+       */
+
+       /*
+        * Método para reestablecer el valor incial de la progresión
+        */
+
+        /*
+         * Método que devuelve el siguiente valor de la progresión
+         */
+
+         /*
+          * Método que regresa el i-ésimo valor de la progresión
+          * 
+          * Empieza en el valor incial y cuenta i-1
+          */
+
+          /*
+           * Método para imprimir los primeros n-valores de la progresión
+           */
+}
